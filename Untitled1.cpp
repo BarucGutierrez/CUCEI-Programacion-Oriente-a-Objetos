6ebe1a5672b9ea86223b15db41beec2be2c8c6c8
Baruc Gutierrez <baruc2006gf@gmail.com>
2025-09-23 12:43:38 -0600

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,54 +1,126 @@
 #include <iostream>
-#include<string>
 
 using namespace std;
 
-class Alumno{
-//atributos
-private:
-    string nombre;
-    string codigo;
-    float calificaciones[5];
-//metodos
+class Figura{
 public:
-    void imprimir();
-    float promedio();
-    Alumno();
-    ~Alumno();
+    int cordx;
+    int cordy;
+
+public:
+    void imprime();
+    double area();
+
 };
 
-void Alumno::imprimir(){
-    if((nombre== " ")||(codigo== " ")){
-       cout<<"ingrese el nombre del alumno: "<<endl;
-       cin>>nombre;
-       cout<<"ingrese el codigo del alumno: "<<endl;
-       cin>>codigo;
-       Alumno::promedio();
+class Cuadrado : public Figura{
+private:
+    double base;
+    double altura;
+
+public:
+    void pedirDatos(){
+        cout<<"ingrese la cordenada en x de la figura: "<<endl;
+        cin>>cordx;
+        cout<<"ingrese la cordenada en y de la figura: "<<endl;
+        cin>>cordy;
+        cout<<"ingrese la base de la figura: "<<endl;
+        cin>>base;
+        cout<<"ingrese la altura de la figura: "<<endl;
+        cin>>altura;
+    };
+
+    double area(){
+        double re;
+        re = base*altura;
+        return re;
+    };
+
+    void imprime(){
+        cout<<"Las cordenadas de la figura son: ("<<cordx<<","<<cordy<<")"<<endl;
+        cout<<"el area de la figura es: "<<area()<<endl;
+    };
+};
+
+class Triangulo : public Figura{
+private:
+    double base;
+    double altura;
+
+public:
+    void pedirDatos(){
+        cout<<"ingrese la cordenada en x de la figura: "<<endl;
+        cin>>cordx;
+        cout<<"ingrese la cordenada en y de la figura: "<<endl;
+        cin>>cordy;
+        cout<<"ingrese la base de la figura: "<<endl;
+        cin>>base;
+        cout<<"ingrese la altura de la figura: "<<endl;
+        cin>>altura;
+    };
+
+    double area(){
+        double re;
+        re = (base*altura)/2;
+        return re;
+    };
+
+    void imprime(){
+        cout<<"Las cordenadas de la figura son: ("<<cordx<<","<<cordy<<")"<<endl;
+        cout<<"el area de la figura es: "<<area()<<endl;
+    };
+};
+
+class Circulo : public Figura{
+private:
+    double radio;
+
+public:
+        void pedirDatos(){
+        cout<<"ingrese la cordenada en x de la figura: "<<endl;
+        cin>>cordx;
+        cout<<"ingrese la cordenada en y de la figura: "<<endl;
+        cin>>cordy;
+        cout<<"ingrese el radio de la figura: "<<endl;
+        cin>>radio;
+    };
+
+    double area(){
+        double re;
+        re = (3.14)*(radio*radio);
+        return re;
     }
-}
-
-float Alumno::promedio(){
-    int i;
-    float promedio=0;
-    for(i=0;i<5;i++){
-        cout<<"ingrese la calificacion "<<i<<": "<<endl;
-        cin>>calificaciones[i];
-        promedio += calificaciones[i];
+    void imprime(){
+        cout<<"Las cordenadas de la figura son: ("<<cordx<<","<<cordy<<")"<<endl;
+        cout<<"el area de la figura es: "<<area()<<endl;
     }
-    promedio = promedio/5;
-    return promedio;
-}
-
-Alumno::Alumno(){
-    nombre=" ";
-    codigo=" ";
-}
-
-Alumno::~Alumno(){
-}
+};
 
 int main(){
-Alumno pedro;
-pedro.imprimir();
+
+    char eleccion;3
+
+    cout<<"ingrese si quiere utilizar el objeto de cubo-triangulo-circulo (c-t-o)"<<endl;
+    cin>>eleccion;
+
+    switch (eleccion){
+
+
+    }
+    Cuadrado cubin;
+    cubin.pedirDatos();
+    cubin.area();
+    cubin.imprime();
+
+    Triangulo picudo;
+    picudo.pedirDatos();
+    picudo.area();
+    picudo.imprime();
+
+    Circulo gordo;
+    gordo.pedirDatos();
+    gordo.area();
+    gordo.imprime();
+
 return 0;
 }
